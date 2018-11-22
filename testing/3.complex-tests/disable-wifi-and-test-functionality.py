e4600266bf01ca7483b4886912e1b54e33d5ef4b
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-11-22 15:10:35 +0200

    Update disable-wifi-and-test-functionality.py

--- testing/3.complex-tests/disable-wifi-and-test-functionality.py
+++ testing/3.complex-tests/disable-wifi-and-test-functionality.py
@@ -57,7 +57,7 @@ def wififoff_toggle_button_toggle_switch():
     # We are turning off the Access Point, so our device will now be in Network
     # Discovery mode. For this case we turn the wifi on by executing the above request.
     # The execute function of spanner library can be used for any post request. 
-    spanner.execute("192.168.2.47:5000/wifioff")
+    testboard.execute("192.168.2.47:5000/wifioff")
     time.sleep(10)
 
     # check PIN state, make sure it's OFF
