a2101fd79704bc2f8fd5763b7f090e083f515cc2
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-11-22 13:13:46 +0200

    Update uart_periph_mock.py

--- testing/1.basic-tests/UART/uart_periph_mock/uart_periph_mock.py
+++ testing/1.basic-tests/UART/uart_periph_mock/uart_periph_mock.py
@@ -21,41 +21,40 @@
 # settings.
 #
 
-import time
-from Spanner import Spanner
 from Testboard import Testboard
 
 testboard = Testboard("testboard_name")
 
 LOREM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum, nulla nec dictum mattis, sem tellus commodo sem, quis faucibus mauris lectus sed sapien. Duis lobortis lacus tellus, at rhoncus tellus pharetra eu. Quisque semper nisl dolor. Donec consequat mauris sit amet ante pulvinar scelerisque. Proin eu interdum augue. Nam vitae neque quis diam lobortis semper. Proin ut maximus augue.Vivamus elementum neque a lacinia fermentum. Morbi ut maximus magna, eget consectetur purus. Vestibulum ut sodales magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus vehicula metus sit amet urna finibus, vel mattis est pharetra. Praesent sed est nulla. Suspendisse pretium blandit felis id pretium. Suspendisse et pretium nulla.Vivamus nisi lorem, commodo id vulputate sed, feugiat et erat. Pellentesque massa sem, interdum non semper vitae, condimentum at augue. Vivamus sit amet convallis urna, vitae mattis arcu. Proin maximus risus in lectus auctor mattis. Proin tempor fermentum vestibulum. Ut sit amet tempus elit, et bibendum eros. Quisque pellentesque ipsum non porta interdum. Aliquam nisl est, aliquet eu tortor eget, pulvinar pulvinar magna. Donec eu purus gravida, lobortis dolor in, lobortis mi. Donec pharetra semper nibh, non eleifend risus aliquam nec.Ut tincidunt, massa finibus vulputate consectetur, quam augue luctus lacus, elementum bibendum risus leo vel velit. Morbi congue velit ac mollis sollicitudin. Integer at vestibulum justo. Vestibulum in gravida purus. Pellentesque eleifend dui quis consectetur rutrum. In faucibus in metus vel tempus. Praesent quis justo tortor. Pellentesque non ornare purus. Phasellus lacus tortor, pharetra eu facilisis in, porta vulputate mi. Suspendisse faucibus fringilla felis, vel sollicitudin eros auctor eget. Aenean auctor vitae ligula non aliquet. Sed eleifend quam velit, a eleifend tortor accumsan vel.Cras euismod, tortor eget ullamcorper sollicitudin, metus erat fermentum ex, et dapibus ipsum sapien a nisl. Cras nec leo porta, congue nisi vel turpis duis."""
 
+
 def chunks(data, chunk_size):
     for i in range(0, len(data), chunk_size):
         yield data[i:i + chunk_size]
 
+
 def char_count_mock():
-
     Serial = Testboard.Serial
-
-    my_procedure = testboard.createProcedure('UART')
+    my_procedure = testboard.createProcedure('UART').\
         # Serial setup
-        .setup(9600, Serial.DATA_BITS_8 | Serial.STOP_BITS_1 | Serial.PARITY_NO)
+        setup(9600, Serial.DATA_BITS_8 | Serial.STOP_BITS_1 | Serial.PARITY_NO).\
         # Wait for the device 'ready\n' flag to initiate communication
-        .doAssertSerialRead('ready\n')
-        .doWait(1000)
+        doAssertSerialRead('ready\n').\
+        doWait(1000)
+
 
     # Send the dummy text
     for piece in chunks(LOREM, my_procedure.MAX_PAYLOAD_LEN):
         my_procedure.doSerialWrite(piece)
 
     # Assert read on the text lengths
-    my_procedure.doSerialWrite('\n')
-                .doAssertSerialRead('%d\n' % (len(LOREM),))
+    my_procedure.doSerialWrite('\n').\
+        doAssertSerialRead('%d\n' % (len(LOREM),))
 
     # Execute the mock function
     spanner.assertEqual(my_procedure.run(), 0)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # Start the test
     char_count_mock()
