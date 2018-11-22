2a598b6aa900836b020e3a00b085031b2150710b
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-11-22 13:03:28 +0200

    Update measure-power-consumption.py

--- testing/3.complex-tests/measure-power-consumption.py
+++ testing/3.complex-tests/measure-power-consumption.py
@@ -25,7 +25,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-testboard = Testboard("MySpannerTester")
+testboard = Testboard("testboard_name")
 # Our Testboard's D3 Pin is connected to a power switching circuit that controls
 # the power going to the device. When we toggle it HIGH, the device will be
 # powered, when LOW, the device will shut down
